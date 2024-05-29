63d3573ae93ce864c5754d01491d47cde3514b87
woyteck <wojciech.hoffmann@gmail.com>
2024-05-29 18:45:18 +0200

    added consumer

--- distance_calculator/consumer.go
+++ distance_calculator/consumer.go
@@ -1,5 +1,48 @@
 package main
 
-type DataConsumer interface {
-	ConsumeData()
+import (
+	"fmt"
+
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/sirupsen/logrus"
+)
+
+type KafkaConsumer struct {
+	consumer  *kafka.Consumer
+	isRunning bool
+}
+
+func NewKafkaConsumer(topic string) (*KafkaConsumer, error) {
+	c, err := kafka.NewConsumer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost",
+		"group.id":          "myGroup",
+		"auto.offset.reset": "earliest",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	c.SubscribeTopics([]string{topic}, nil)
+
+	return &KafkaConsumer{
+		consumer: c,
+	}, nil
+}
+
+func (c *KafkaConsumer) Start() {
+	logrus.Info("kafka transport started")
+	c.isRunning = true
+	c.readMessageLoop()
+}
+
+func (c *KafkaConsumer) readMessageLoop() {
+	for c.isRunning {
+		msg, err := c.consumer.ReadMessage(-1)
+		if err != nil {
+			logrus.Errorf("kafka consume error: %s", err)
+			continue
+		} else {
+			fmt.Println(msg)
+		}
+	}
 }
--- distance_calculator/main.go
+++ distance_calculator/main.go
@@ -1,36 +1,18 @@
 package main
 
-import (
-	"fmt"
-	"time"
+import "log"
 
-	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
-)
+// type DistanceCalculator struct {
+// 	consumer DataConsumer
+// }
 
-type DistanceCalculator struct {
-	consumer DataConsumer
-}
+const kafkaTopic = "obudata"
 
 func main() {
-	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "localhost",
-		"group_id":          "myGroup",
-		"auto.offset.reset": "earliest",
-	})
+	kafkaConsumer, err := NewKafkaConsumer(kafkaTopic)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	c.SubscribeTopics([]string{"myTopic", "^aRegex.*[Tt]opic"}, nil)
-
-	run := true
-
-	for run {
-		msg, err := c.ReadMessage(time.Second)
-		if err != nil {
-			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
-		} else if !err.(kafka.Error).IsTimeout() {
-			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
-		}
-	}
+	kafkaConsumer.Start()
 }
